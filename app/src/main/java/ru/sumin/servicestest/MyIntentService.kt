693863658b97d37b4a219a2730863098d5601378
alexanderrodin <pass.9157@gmail.com>
2023-04-04 11:00:17 +0300

    Intent Service

--- app/src/main/java/ru/sumin/servicestest/MyIntentService.kt
+++ app/src/main/java/ru/sumin/servicestest/MyIntentService.kt
@@ -1,19 +1,16 @@
 package ru.sumin.servicestest
 
-import android.app.Notification
+import android.app.IntentService
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
-import kotlinx.coroutines.*
 
-class MyForegroundService : Service() {
-    private val coroutineScope = CoroutineScope(Dispatchers.Main)
+class MyIntentService : IntentService(NAME) {
 
     override fun onCreate() {
         super.onCreate()
@@ -22,34 +19,27 @@ class MyForegroundService : Service() {
         log("onCreate")
     }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        log("onStartCommand")
-        coroutineScope.launch {
-            for (i in 0 until 10) {
-                delay(1000)
-                log("timer: $i")
-            }
-            stopSelf()
+    override fun onHandleIntent(p0: Intent?) {
+        log("onHandleIntent")
+        for (i in 0 until 10) {
+            Thread.sleep(1000)
+            log("timer: $i")
         }
-        return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        coroutineScope.cancel()
         log("onDestroy")
     }
 
-    override fun onBind(p0: Intent?): IBinder? {
-        TODO("Not yet implemented")
-    }
 
     private fun log(message: String) {
-        Log.d("SERVICE_TAG", "MyForegroundService: $message")
+        Log.d("SERVICE_TAG", "MyIntentService: $message")
     }
 
     private fun createNotificationChanel() {
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager =
+            getSystemService(Service.NOTIFICATION_SERVICE) as NotificationManager
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationChannel = NotificationChannel(
                 CHANEL_ID,
@@ -71,8 +61,10 @@ class MyForegroundService : Service() {
         private const val CHANEL_ID = "chanel_id"
         private const val CHANEL_NAME = "chanel_name"
         private const val NOTIFICATION_ID = 1
+        private const val NAME = "MyIntentService"
+
         fun newIntent(context: Context): Intent {
-            return Intent(context, MyForegroundService::class.java)
+            return Intent(context, MyIntentService::class.java)
         }
     }
 }
